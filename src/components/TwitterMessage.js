4b9341847a2d708a0df6729b94092264e8786e33
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-26 13:49:33 -0500

    commit tree 5788ff420a3271e4f4ce9f96a4febfabd2720e39

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,32 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+      charsNum: 0,
+    };
   }
+
+  handleChange = (event) => {
+    this.setState({
+      message: event.target.value,
+    });
+  };
 
   render() {
     return (
       <div>
+        <p>
+          Characters left: {this.props.maxChars - this.state.message.length}
+        </p>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={this.handleChange}
+          value={this.state.message}
+        />
       </div>
     );
   }
